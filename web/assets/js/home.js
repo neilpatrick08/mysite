6faab23ca23a9c54c48d9fe6cde3ce6b26fcf930
EC2 Default User <ec2-user@ip-172-31-19-176.ap-southeast-1.compute.internal>
2016-07-29 01:37:21 +0000

    update slick

--- web/assets/js/home.js
+++ web/assets/js/home.js
@@ -3,5 +3,11 @@ $('img.lazy').lazyload();
 $('.slicker').slick({
 	"slidesToScroll": 1, 
 	"adaptiveHeight": true,
-	"autoplay": true
+	"autoplay": true,
+	"responsive" : [
+    	{
+     		 breakpoint: 480,
+     		 settings: 'unslick'
+     	 }
+    	]
 });
